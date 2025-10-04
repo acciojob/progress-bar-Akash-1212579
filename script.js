a8a9c6bfed2f322d3b004c857e953d5d22716ec5
Akash-1212579 <dalbhanjanakash1@gmail.com>
2025-10-04 16:10:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ let next = document.getElementById("next");
 let prev = document.getElementById("prev");
 let activeCircles = document.querySelectorAll(".circle");
 let current = 0; // start from index 0
-
+ activeCircles[0].classList.add("active");
+        current++;
+ if (current <= 1) {
+        prev.disabled = true; // disable at the start
+    }
 next.addEventListener("click", () => {
     if (current < activeCircles.length) {
         activeCircles[current].classList.add("active");
@@ -20,8 +24,8 @@ prev.addEventListener("click", () => {
         current--; // move back first
         activeCircles[current].classList.remove("active");
     }
-
-    if (current === 0) {
+   
+    if (current <= 1) {
         prev.disabled = true; // disable at the start
     }
     next.disabled = false; // re-enable next if moving back
